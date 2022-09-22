ee83f53cba9d27ab7209d081064de82dea47d5b1
Akinkuowo Oluwaseun <akinseun1990@gmail.com>
2022-09-22 03:05:14 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import Header from './components/Header/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    
+      <Header />
+      {/* Home */}
+
+    </div>
+  );
+}
+
+export default App;
