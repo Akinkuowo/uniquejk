6883c7613a5310139f79ec16b57e918d3b3d3512
Akinkuowo Oluwaseun <akinseun1990@gmail.com>
2022-09-22 03:08:37 +0100

    Add files via upload

---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA9XLB1bv1lHXOjZujQf4iWfl3IWfuB2pw",
+    authDomain: "uniquejk-94560.firebaseapp.com",
+    projectId: "uniquejk-94560",
+    storageBucket: "uniquejk-94560.appspot.com",
+    messagingSenderId: "9796689907",
+    appId: "1:9796689907:web:bcbbc0a5b959e364debc86",
+    measurementId: "G-7BCBGM2Y38"
+  };
